1045f504b19e5bb0baead35a84d50acb95790d74
Yassine Ennassiri <yassine.ennassiri95@gmail.com>
2026-08-28 15:51:49 +0200

    save messages to database via prisma

--- api/messages.js
+++ api/messages.js
@@ -1,8 +1,19 @@
+const { PrismaClient } = require("@prisma/client");
+const { PrismaNeon } = require("@prisma/adapter-neon");
+
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
 module.exports = async function handler(req, res) {
-    if (req.method === 'POST') {
-        const text = req.body.text;
-        return res.status(200).json({ received: text });
-    }
-    
-    res.status(405).json({ error: 'Method not allowed' });
+  if (req.method === "POST") {
+    const text = req.body.text;
+
+    const message = await prisma.message.create({
+      data: { text: text },
+    });
+
+    return res.status(200).json(message);
+  }
+
+  res.status(405).json({ error: "Method not allowed" });
 };
